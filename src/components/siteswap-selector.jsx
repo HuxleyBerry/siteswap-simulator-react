6d40f953dcd9a1884dc99db235042d9964feddd3
HuxleyBerry <huxley.berry@gmail.com>
2024-06-25 21:31:33 +0800

    highlight input field if invalid input

--- src/components/siteswap-selector.jsx
+++ src/components/siteswap-selector.jsx
@@ -1,9 +1,11 @@
 'use client'
 import { useState} from 'react';
+import { parseSiteswap} from '../utils/siteswap-validation'
 
 export default function SiteswapSelector({clickHandler}) {
 
     const [input, setInput] = useState("")
+    const [inputHighlighted, setInputHighlighted] = useState(false);
     const asyncExamples = ["534", "12345", "441", "3", "4", "5", "6", "7", "744", "633", "1357", "51", "17", "53", "423", "525", "50505", "5551", "7131", "561", "4453", "612", "73", "312", "531", "61616", "663", "5241", "5313", "5524", "7333", "7571", "45141", "52512", "56414"];
     const syncExamples = ["(2x,4x)", "(4,2x)(2x,4)", "(4,4)", "(4,4)(4x,0)(4,4)(0,4x)", "(4,6x)(2x,4)", "(4x,2x)", "(4x,2x)(4,2x)(2x,4x)(2x,4)", "(4x,4x)", "(4x,6)(6,4x)", "(6,4x)(4x,2)", "(6x,2x)", "(6x,2x)(2x,6x)", "(6x,4)(4,2x)(4,6x)(2x,4)", "(6x,4)(4,6x)", "(6x,4x)", "(6x,6x)(2x,2x)", "(2x,2x)", "(8,2x)(4,2x)(2x,8)(2x,4)"];
     const multiplexExamples = ["[54]24", "[43]1421", "4[43]1", "[32]", "[43]23", "[43][32]3", "[31]", "(2,4)([4x4],2x)", "(2,4x)([4x4],2)", "(2,6)([6x6],2x)(6x,2x)"];
@@ -15,9 +17,18 @@ export default function SiteswapSelector({clickHandler}) {
         setInput("");
     }
 
+    function submitInput() {
+        clickHandler(input);
+        if (parseSiteswap(input).length === 0) {
+            setInputHighlighted(true);
+        } else {
+            setInput("");
+        }
+    }
+
     return <div className="flex flex-row items-center justify-content">
-            <input value={input} placeholder={"Enter siteswap"} className="w-full p-2 h-10 bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-md" onChange={(event) => setInput(event.target.value)}/>
-            <button className='bg-blue-400 text-lg text-black px-4 py-2 rounded-lg mx-2' onClick={() => {clickHandler(input); setInput("")}}>Go!</button>
+            <input value={input} placeholder={"Enter siteswap"} className={`w-full p-2 h-10 bg-gray-50 border ${inputHighlighted ? "outline outline-2 outline-offset-0 outline-red-400" : null} border-gray-300 ${inputHighlighted ? "text-red-400" : "text-gray-900"} text-md rounded-md`} onChange={(event) => setInput(event.target.value)} onClick={() => setInputHighlighted(false)}/>
+            <button className='bg-blue-400 text-lg text-black px-4 py-2 rounded-lg mx-2' onClick={submitInput}>Go!</button>
             <button className='bg-zinc-300 text-lg text-black p-2 rounded-lg' onClick={getRandomSiteswap}>Random</button>
         </div>
 }
